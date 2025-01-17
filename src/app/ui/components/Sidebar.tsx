a64fb7210a15ee6dcec81a97951e7dbc25738d9a
Zin Myo Win <mgzinmyowin12@gmail.com>
2025-01-17 23:24:39 +0700

    Add SVG icons for sidebar navigation: implement active and inactive states for Home, Products, and Cart links; enhance visual representation with images.

--- src/app/ui/components/Sidebar.tsx
+++ src/app/ui/components/Sidebar.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
 import { auth } from "../../../../auth";
+import HomeActive from '../../../../public/icons/home-active.svg'
+import HomeInactive from '../../../../public/icons/home-inactive.svg'
+import ProductActive from '../../../../public/icons/product-active.svg'
+import ProductInactive from '../../../../public/icons/product-inactive.svg'
+import CartActive from '../../../../public/icons/cart-filled.svg'
+import CartInactive from '../../../../public/icons/cart-unfilled.svg'
+import Image from "next/image";
 
 export default async function Sidebar() {
   const session = await auth();
@@ -12,7 +19,8 @@ export default async function Sidebar() {
         <nav className="w-full mt-2">
           <ul className='flex flex-col space-y-2 font-semibold  hover:*:bg-accent *:p-2 *:rounded-sm'>
             <li>
-              <Link href='/' className=''>Home</Link>
+              <Link className="flex flex-row items-center justify-between px-1" href='/'> <Image src={HomeInactive} alt="" width={30} height={30}>
+                </Image>Home</Link>
             </li>
             <li>
               <Link href='/products' className=''>Products</Link>
