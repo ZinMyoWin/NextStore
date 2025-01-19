e004ca8e06886de20cda4c08c8cba059d75e113c
Zin Myo Win <mgzinmyowin12@gmail.com>
2025-01-19 17:31:13 +0700

    Add new icons for home and product states, and update cart icon for improved visuals

--- src/app/ui/components/Sidebar.tsx
+++ src/app/ui/components/Sidebar.tsx
@@ -14,47 +14,59 @@ export default async function Sidebar() {
   console.log("Session in Sidebar.tsx", session);
 
   return (
-    <div className='w-52 bg-primary text-background h-screen fixed left-0 top-0 shadow-lg shadow-black'>
+    <div className='w-52 bg-background text-text h-screen fixed left-0 top-0 shadow-lg '>
       <div className='p-4'>
         <h1 className='text-2xl font-extrabold mb-8'>NexE</h1>
         <nav className='w-full mt-2'>
-          <ul className='flex flex-col space-y-2 font-semibold  hover:*:bg-accent *:p-2 *:rounded-sm'>
-            <li>
+          <ul className='flex flex-col space-y-2 font-semibold  hover:*:bg-secondary *:p-2 *:rounded-sm '>
+            <li className='gap-2'>
               <Link
-                className='flex flex-row items-center justify-between'
+                className='flex flex-row items-center justify-between gap-2'
                 href='/'
               >
-                <Image src={HomeInactive} alt='' width={30} height={30}></Image>
-                <p className="w-3/4">Home</p>
+                <Image src={HomeInactive} alt='' className='w-1/4'></Image>
+                <p className='w-3/4 text-lg'>Home</p>
               </Link>
             </li>
-            <li>
-              <Link href='/products' className='flex flex-row items-center justify-between px-1'>
-              <Image src={ProductInactive} alt='' width={30} height={30}></Image>
-              <p className="w-3/4">Product</p>
+            <li className='gap-2'>
+              <Link
+                href='/products'
+                className='flex flex-row items-center justify-between gap-2 '
+              >
+                <Image src={ProductInactive} alt='' className='w-1/4'></Image>
+                <p className='w-3/4 text-lg'>Product</p>
               </Link>
             </li>
-            <li>
-              <Link href='/cart' className='flex flex-row items-center justify-between px-1'>
-              <Image src={CartInactive} alt='' width={30} height={30}></Image>
-              <p className="w-3/4">Cart</p>
+            <li className='gap-2'>
+              <Link
+                href='/cart'
+                className='flex flex-row items-center justify-between gap-2   '
+              >
+                <Image src={CartInactive} alt='' className='w-1/4'></Image>
+                <p className='w-3/4 text-lg'>Cart</p>
               </Link>
             </li>
             {/* <li>
               <Link href='/checkout' className=''>CheckOut</Link>
             </li> */}
             {session?.user ? (
-              <li>
-                <Link href='/auth/signout' className='flex flex-row items-center justify-between px-1'>
-                <Image src={Login} alt='' width={30} height={30}></Image>
-                <p className="w-3/4">Log Out</p>
+              <li >
+                <Link
+                  href='/auth/signout'
+                  className='flex flex-row items-center justify-between gap-2'
+                >
+                  <Image src={Login} alt='' className='w-1/4'></Image>
+                  <p className='w-3/4 text-lg'>Log Out</p>
                 </Link>
               </li>
             ) : (
-              <li>
-                <Link href='/auth/signin' className='flex flex-row items-center justify-between px-1'>
-                <Image src={Login} alt='' width={30} height={30}></Image>
-                <p className="w-3/4">Login</p>
+              <li >
+                <Link
+                  href='/auth/signin'
+                  className='flex flex-row items-center justify-between gap-2 '
+                >
+                  <Image src={Login} alt='' width={30} height={30}></Image>
+                  <p className='w-3/4 text-lg '>Login</p>
                 </Link>
               </li>
             )}
