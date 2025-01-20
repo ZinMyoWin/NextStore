a5fb45c02def436c1c36a160e0eb6a3ee8e7baee
Zin Myo Win <mgzinmyowin12@gmail.com>
2025-01-20 16:01:54 +0700

    Update ProductCard component: add rounded corners to image styling for improved aesthetics

--- src/app/ui/components/ProductCard.tsx
+++ src/app/ui/components/ProductCard.tsx
@@ -32,7 +32,7 @@ export default function ProductCard({
         alt='product-image'
         width={279}
         height={286.48}
-        className='justify-self-center'
+        className='justify-self-center rounded-md'
       ></Image>
 
       <div className='h-fit gap-2 flex flex-col items-start p-2'>
