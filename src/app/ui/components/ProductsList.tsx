964654352360493211128b9179c7e09b5c9e09c3
Zin Myo Win <mgzinmyowin12@gmail.com>
2025-01-20 22:51:40 +0700

    Refactor ProductsList component: update layout and styling for better responsiveness

--- src/app/ui/components/ProductsList.tsx
+++ src/app/ui/components/ProductsList.tsx
@@ -8,15 +8,15 @@ export default function ProductsList({ products }: { products: Product[] }) {
   const { session } = useCart();
 
   return (
-    <div className='grid grid-cols-4 w-full mt-8 justify-center ml-auto mr-auto gap-12 justify-items-center'>
+    <div className='grid grid-cols-4 w-full mt-8 justify-center ml-auto mr-auto gap-12 justify-items-center *:h-96'>
       {products.map(( product) => (
         <ProductCard key={product._id} {...product} type='product' />
       ))}
       {session?.user?.role === "user" && (
-        <div>
+        <div className="w-full p-2">
           <Link
             href={"/products/add-product"}
-            className='h-2/3 w-60 mt-1 hover:bg-accent bg-secondary hover:text-background grid grid-cols-1 justify-items-center items-center border-dashed border-2 border-accent rounded-md group transition-all ease-in-out duration-300 cursor-pointer'
+            className='h-full mt-1 hover:bg-accent bg-secondary hover:text-background grid grid-cols-1 justify-items-center items-center border-dashed border-2 border-accent rounded-md group transition-all ease-in-out duration-300 cursor-pointer'
           >
             <div className=''>Add Product</div>
           </Link>
