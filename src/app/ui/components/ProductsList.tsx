6cf6c14230b886054403e2c26c8185b0a3b36863
Zin Myo Win <mgzinmyowin12@gmail.com>
2025-01-20 21:31:15 +0700

    Refactor ProductsList component: update ProductCard key to use product _id for consistency in identification

--- src/app/ui/components/ProductsList.tsx
+++ src/app/ui/components/ProductsList.tsx
@@ -9,8 +9,8 @@ export default function ProductsList({ products }: { products: Product[] }) {
 
   return (
     <div className='grid grid-cols-4 w-full mt-8 justify-center ml-auto mr-auto gap-12 justify-items-center'>
-      {products.map(( product, id) => (
-        <ProductCard key={id} {...product} type='product' />
+      {products.map(( product) => (
+        <ProductCard key={product._id} {...product} type='product' />
       ))}
       {session?.user?.role === "user" && (
         <div>
