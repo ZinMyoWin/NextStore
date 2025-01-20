a11ff1dc63b24a41c8dc8189b3cdb2b76a850978
Zin Myo Win <mgzinmyowin12@gmail.com>
2025-01-20 21:29:42 +0700

    Add _id field to Product interface and comment out existing product data

--- src/app/product-data.ts
+++ src/app/product-data.ts
@@ -1,4 +1,5 @@
 export interface Product {
+  _id: string;
   id: string;
   imageUrl: string;
   name: string;
@@ -7,31 +8,31 @@ export interface Product {
   price: number;
 }
 
-export const products: Product[] = [
-  {
-    id: "1",
-    name: "ÅKERFIBBLA",
-    imageUrl: "akerfibbla-duvet-cover-and-pillo.webp",
-    shortDescription:
-      "Duvet cover and pillowcase, white black/check, 150x200/50x80 cm",
-    description: "",
-    price: 29,
-  },
-  {
-    id: "2",
-    name: "BREDVECKLARE",
-    imageUrl: "bredvecklare-duvet-cover-and-2-p.webp",
-    shortDescription:
-      "Duvet cover and 2 pillowcases, white blue/check, 200x200/50x80 cm",
-    description: "",
-    price: 29,
-  },
-  {
-    id: "3",
-    name: "BRUKSVARA",
-    imageUrl: "bruksvara-4-piece-bedlinen-set-l.webp",
-    shortDescription: "4-piece bedlinen set, light blue/geometric, 240x220 cm",
-    description: "",
-    price: 40,
-  },
-];
+// export const products: Product[] = [
+//   {
+//     id: "1",
+//     name: "ÅKERFIBBLA",
+//     imageUrl: "akerfibbla-duvet-cover-and-pillo.webp",
+//     shortDescription:
+//       "Duvet cover and pillowcase, white black/check, 150x200/50x80 cm",
+//     description: "",
+//     price: 29,
+//   },
+//   {
+//     id: "2",
+//     name: "BREDVECKLARE",
+//     imageUrl: "bredvecklare-duvet-cover-and-2-p.webp",
+//     shortDescription:
+//       "Duvet cover and 2 pillowcases, white blue/check, 200x200/50x80 cm",
+//     description: "",
+//     price: 29,
+//   },
+//   {
+//     id: "3",
+//     name: "BRUKSVARA",
+//     imageUrl: "bruksvara-4-piece-bedlinen-set-l.webp",
+//     shortDescription: "4-piece bedlinen set, light blue/geometric, 240x220 cm",
+//     description: "",
+//     price: 40,
+//   },
+// ];
